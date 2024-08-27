6f4333870ea1801d31c4ea7790afddb814d73457
Guilherme Kenzo <guilhermekenzos@gmail.com>
2024-08-27 11:00:49 -0300

    updating and adding new data

---
+++ README.md
@@ -0,0 +1,36 @@
+# Crawler and Text Extractions for the "diarios" of the Brazilian Constituent assembly (1987-1988)
+
+This project was setup to run on a nix [devenv](https://devenv.sh/) and poetry. I believe, however, that it using poetry alone should suffice but this setup was not tested.
+
+## Requirements
+
+You should have at least [mupdf](https://mupdf.com/) and [poetry](https://python-poetry.org/) installed.
+
+## How to crawl the data
+
+To execute the crawler, run the following commands:
+
+```bash
+mkdir pdfs # Creates pdf folder
+poetry install # installs python dependencies
+python crawler.py 1987-01-01 1987-12-31 # crawl first year
+python crawler.py 1988-01-01 1988-12-31 # crawl second year
+ 
+```
+
+The PDFs of the "diarios" will be downloaded to the pdfs folder.
+
+## How to extract text from pdfs
+
+```bash
+mkdir extractions
+python extract.py # this will iterate over files in /pdfs and extract them into /extractions
+```
+
+The dates can be adjusted to reflect your particular needs. The maximum range is 1 year because of the crawled sites limitations. 
+
+The extracted text files will be available at the "extractions" folder.
+
+## License
+
+This project is licensed under the MIT license. 
--- crawler.py
+++ crawler.py
@@ -2,8 +2,10 @@ from requests_html import HTMLSession
 from requests import exceptions
 from time import sleep
 from glob import glob
+from datetime import datetime
 import os
 import re
+import click
 
 
 SESSION = HTMLSession()
@@ -12,6 +14,7 @@ def _get_w_retry(url, attempts=1, max_attempts=10):
     if attempts >= max_attempts:
         return None
     try:
+        print(url)
         r = SESSION.get(url)
     except exceptions.ConnectionError as e:
         print(e)
@@ -23,8 +26,11 @@ def _check_if_doc_already_exists(file_name):
     return file_name in existing_files
 
 
-def get_init_page():
-    return SESSION.get("https://imagem.camara.gov.br/prepara.asp?selDataIni=02/02/1987&selDataFim=05/10/1988&opcao=1&selCodColecaoCsv=R")
+def get_init_page(initial_date: datetime, final_date: datetime):
+    return SESSION.get(("https://imagem.camara.gov.br/prepara.asp?selDataIni="
+                        f"{initial_date.strftime('%d/%m/%Y')}"
+                        f"&selDataFim={final_date.strftime('%d/%m/%Y')}"
+                        "&opcao=1&selCodColecaoCsv=R"))
 
 def get_relevant_links(init_page):
     days = init_page.html.find('td.calWeekDaySel')
@@ -36,7 +42,12 @@ def get_relevant_links(init_page):
 
 def get_page_content(url):
     r = SESSION.get(url)
-    pdf_url = re.search(r'http.+?pdf#page=', r.text).group()
+    try:
+        pdf_url = re.search(r'http://.+?pdf#page=', r.text, flags=re.I).group()
+    except AttributeError as e:
+        raise Exception("Got an error: {error}. Extraction of the url from the page failed." 
+               " The page content is: " + r.text.format(error=e))
+        
     print(f"Downloading pdf at {pdf_url}")
     r = _get_w_retry(pdf_url)
     file_name = pdf_url.split('/')[-1].strip('#page=')
@@ -46,8 +57,11 @@ def save_pdf_file(file_name, content):
     with open(os.path.join("pdfs", file_name), 'wb') as f:
         f.write(content)
 
-def main():
-    init_page = get_init_page()
+@click.command()
+@click.argument('initial_date', type=click.DateTime())
+@click.argument('final_date', type=click.DateTime())
+def main(initial_date, final_date):
+    init_page = get_init_page(initial_date=initial_date, final_date=final_date)
     urls = get_relevant_links(init_page)
     for i in urls:
         content, file_name = get_page_content(i)
